5805f6cba031f1ab303d5642e93e0a661526a7bd
+ <+>
2024-12-12 08:25:31 +0300

    feat:add views

--- songsproject/accounts/views.py
+++ songsproject/accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import CustomUser
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import UserSerializer, LoginSerializer
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class SignupView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+class LoginView(APIView):
+
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+
+            return Response({
+                "message": "Logged in successfully",
+                "access_token": access_token,
+                "refresh_token": str(refresh),
+                "role": user.role,
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class UserListView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+class AddUserView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+class DeleteUserView(APIView):
+
+    def delete(self, request, pk):
+        try:
+            user = CustomUser.objects.get(pk=pk)
+        except CustomUser.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        user.delete()
+        return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
