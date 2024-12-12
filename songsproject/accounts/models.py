4ecbca65df010a01198c17a110c3de78271e9ffa
+ <+>
2024-12-12 08:23:02 +0300

    refactor:modify models and serializers

--- songsproject/accounts/models.py
+++ songsproject/accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, default='trainer')
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customer_set',
+        blank=True,
+        help_text='The groups the user belongs to',
+        verbose_name='groups',
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customer_set',
+        blank=True,
+        help_text='Specific permissions for this user',
+        verbose_name='user permissions',
+    )
+
+    def __str__(self):
+        return self.username
---
+++ songsproject/accounts/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+
+        user.role = 'trainer' if CustomUser.objects.count() == 0 else 'user'
+        user.save()
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        try:
+            user = CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            raise serializers.ValidationError("Invalid email or password")
+
+        if not user.check_password(password):
+            raise serializers.ValidationError("Invalid email or password")
+
+        attrs['user'] = user
+        return attrs
